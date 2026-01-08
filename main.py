14ee5ae49723535f0f3f0a891292ed54cb60e38b
Harsh Patel <code.by.hp@gmail.com>
2026-01-08 10:12:06 +0530

    implemented pushover notify functionality

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv
 import os
 from openai import AzureOpenAI
 from pypdf import PdfReader
+import requests
+from tools import handle_tool_calls, TOOLS
 
 load_dotenv()
 
+
 client = AzureOpenAI(azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"))
+
 
 if os.path.exists("static/profile.pdf"):
     reader = PdfReader("static/profile.pdf")
@@ -25,28 +29,41 @@ else:
 
 NAME = "Harsh Patel"
 
-SYSTEM_PROMPT = f"""You are acting as {NAME}. Answer questions on {NAME}'s website related to their career, skills, and experience.
-Represent {NAME} faithfully using the provided context. Be professional and engaging.
-If you don't know the answer, say so.
-## Summary:
-{SUMMARY}
-## LinkedIn Profile:
-{LINKEDIN}
-With this context, chat with the user, staying in character as {NAME}."""
+SYSTEM_PROMPT = f"You are acting as {NAME}. You are answering questions on {NAME}'s website, \
+particularly questions related to {NAME}'s career, background, skills and experience. \
+Your responsibility is to represent {NAME} for interactions on the website as faithfully as possible. \
+You are given a summary of {NAME}'s background and LinkedIn profile which you can use to answer questions. \
+Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+SYSTEM_PROMPT += f"\n\n## Summary:\n{SUMMARY}\n\n## LinkedIn Profile:\n{LINKEDIN}\n\n"
+SYSTEM_PROMPT += f"With this context, please chat with the user, always staying in character as {NAME}."
 
 
-def chat(query, history):
-    # History is automatically handled by Gradio.
-    # History -> [{'role': 'user', 'content': 'hi'}, {'role': 'assistant', 'content': 'hello'}]
+def chat(message, history):
     messages = (
         [{"role": "system", "content": SYSTEM_PROMPT}]
         + history
-        + [{"role": "user", "content": query}]
+        + [{"role": "user", "content": message}]
     )
+    done = False
+    while not done:
 
-    response = client.chat.completions.create(
-        model=str(os.getenv("AZURE_OPENAI_DEPLOYMENT")), messages=messages
-    )
+        response = client.chat.completions.create(
+            model="gpt-4o-mini", messages=messages, tools=TOOLS
+        )
+
+        finish_reason = response.choices[0].finish_reason
+
+        if finish_reason == "tool_calls":
+            message = response.choices[0].message
+            tool_calls = message.tool_calls
+            results = handle_tool_calls(tool_calls)
+            messages.append(message)
+            messages.extend(results)
+        else:
+            done = True
     return response.choices[0].message.content
 
 
---
+++ tools.py
@@ -0,0 +1,99 @@
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+load_dotenv()
+
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_url = "https://api.pushover.net/1/messages.json"
+
+
+def push(message):
+    print(f"Push: {message}")
+    payload = {"user": pushover_user, "token": pushover_token, "message": message}
+    requests.post(pushover_url, data=payload)
+
+
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording interest from {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
+
+
+def record_unknown_question(question):
+    push(f"Recording {question} asked that I couldn't answer")
+    return {"recorded": "ok"}
+
+
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "The email address of this user",
+            },
+            "name": {
+                "type": "string",
+                "description": "The user's name, if they provided it",
+            },
+            "notes": {
+                "type": "string",
+                "description": "Any additional information about the conversation that's worth recording to give context",
+            },
+        },
+        "required": ["email"],
+        "additionalProperties": False,
+    },
+}
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered",
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False,
+    },
+}
+
+TOOLS = [
+    {"type": "function", "function": record_user_details_json},
+    {"type": "function", "function": record_unknown_question_json},
+]
+
+
+def handle_tool_calls(tool_calls):
+    tool_mapping = {
+        "record_user_details": record_user_details,
+        "record_unknown_question": record_unknown_question,
+    }
+
+    results = []
+    for tool_call in tool_calls:
+        tool_name = tool_call.function.name
+        arguments = json.loads(tool_call.function.arguments)
+        print(f"Tool called: {tool_name}", flush=True)
+
+        tool = tool_mapping.get(tool_name)
+        if not tool:
+            raise ValueError(f"Unknown tool: {tool_name}")
+
+        result = tool(**arguments)
+        results.append(
+            {
+                "role": "tool",
+                "content": json.dumps(result),
+                "tool_call_id": tool_call.id,
+            }
+        )
+    return results
